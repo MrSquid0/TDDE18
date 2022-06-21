7880ddc2a5e443094dc287f5fa8e39f019950ba3
Gonzalo <gonzalo0@correo.ugr.es>
2022-06-21 18:06:35 +0200

    Done's removed. Final version of lab4 and lab5 done. Also, files generated by cLion removed with .gitignore

--- Lab1/cmake-build-debug/CMakeFiles/Lab1.dir/InputAndOutput.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/Lab1.dir/InputAndOutput.cpp.o: \
- /home/gonzalo/Escritorio/Linux/TDDE18/Lab1/InputAndOutput.cpp \
- /usr/include/stdc-predef.h
--- Lab1/cmake-build-debug/CMakeFiles/Lab1.dir/WordLength.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/Lab1.dir/WordLength.cpp.o: \
- /home/gonzalo/Escritorio/Linux/TDDE18/Lab1/WordLength.cpp \
- /usr/include/stdc-predef.h
